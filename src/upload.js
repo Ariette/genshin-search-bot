a193372a7e24a7ad212bb9e25f34e621f428752c
Ariette <love.mary123@gmail.com>
2023-06-14 08:13:34 +0000

    Update upload command

--- src/upload.js
+++ src/upload.js
@@ -5,7 +5,7 @@ fs.readdir('./data', (err, lists) => {
   if (err) throw err;
   const keys = lists.map((w) => w.replace('.json', ''));
   for (const key of keys) {
-    if (shell.exec(`npx wrangler kv:key put --binding=DB ${key} --path ./data/${key}.json`).code !== 0) {
+    if (shell.exec(`wrangler kv:key put --binding=DB ${key} --path ./data/${key}.json`).code !== 0) {
       shell.echo('Error: command failed');
       shell.exit(1);
     }
